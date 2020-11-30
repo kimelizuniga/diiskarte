3825253e0c5c35227055e6f20d3e849d4fd91582
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-11-29 23:51:33 -0500

    Add modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 let roofingContent  = document.getElementById("roofing-gallery"),
     deckContent     = document.getElementById("deck-gallery"),
-    interiorContent = document.getElementById("interior-gallery");
+    interiorContent = document.getElementById("interior-gallery"),
+    modalContainer  = document.getElementById("modal-container"),
+    innerCarousel   = document.getElementById("inner-carousel"),
+    imageModal      = document.getElementById("modal-image"),
+    roofImageList   = document.getElementsByClassName("roof-images");
 
 
 /* Display all images for roofing */    
@@ -8,9 +12,10 @@ for(let i = 0; i < roofImages.length; i++){
     roofingContent.innerHTML += 
     `
     <article class="col-6 col-md-4 col-lg-3">
-    <img src="${roofImages[i].source}" alt="${roofImages[i].alt}">
+    <img class="roof-images" onclick="modalShow();" src="${roofImages[i].source}" alt="${roofImages[i].alt}">
     </article>
     `;
+    
 }
 
 /* Display all images for deck and fences */
@@ -18,7 +23,7 @@ for(let i = 0; i < deckImages.length; i++){
     deckContent.innerHTML += 
     `
     <article class="col-6 col-md-4 col-lg-3">
-    <img src="${deckImages[i].source}" alt="${deckImages[i].alt}">
+    <img class="deck-images" onclick="modalShow();" src="${deckImages[i].source}" alt="${deckImages[i].alt}">
     </article>
     `;
 }
@@ -28,7 +33,39 @@ for(let i = 0; i < interiorImages.length; i++){
     interiorContent.innerHTML += 
     `
     <article class="col-6 col-md-4 col-lg-3">
-    <img src="${interiorImages[i].source}" alt="${interiorImages[i].alt}">
+    <img class="interior-images" onclick="modalShow();" src="${interiorImages[i].source}" alt="${interiorImages[i].alt}">
     </article>
     `;
 }
+
+/* MODAL */
+
+function modalShow() {
+    modalContainer.style.display = "block";
+}
+
+$(document).on('click','.roof-images',function(){
+    //do something
+    imageModal.src = this.src;
+})
+
+$(document).on('click','.deck-images',function(){
+    //do something
+    imageModal.src = this.src;
+})
+
+$(document).on('click','.interior-images',function(){
+    //do something
+    imageModal.src = this.src;
+})
+
+$(document).on('click','#exit',function(){
+    //do something
+    modalContainer.style.display = "none";
+})
+
+window.onclick = function(event) {
+    if (event.target == modalContainer) {
+      modalContainer.style.display = "none";
+    }
+  }
