5332c23394696142668b41556d56f031f36cd642
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-12-01 13:27:15 -0500

    Improve gallery slideshow

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,50 @@
 let roofingContent = document.getElementById("roofing-gallery"),
-    deckContent = document.getElementById("deck-gallery"),
-    interiorContent = document.getElementById("interior-gallery"),
-    modalContainer = document.getElementById("modal-container"),
-    innerCarousel = document.getElementById("inner-carousel"),
-    leftArrow = document.getElementById("left"),
-    rightArrow = document.getElementById("right"),
-    slideImages = document.getElementsByClassName("mySlides"),
-    modalContainerD = document.getElementById("modal-container-deck"),
-    innerCarouselD = document.getElementById("inner-carousel-deck"),
-    leftArrowD = document.getElementById("left-deck"),
-    rightArrowD = document.getElementById("right-deck"),
-    slideImagesD = document.getElementsByClassName("mySlidesD"),
-    modalContainerI = document.getElementById("modal-container-interior"),
-    innerCarouselI = document.getElementById("inner-carousel-interior"),
-    leftArrowI = document.getElementById("left-interior"),
-    rightArrowI = document.getElementById("right-interior"),
-    slideImagesI = document.getElementsByClassName("mySlidesI"),
-    roofImageList = document.getElementsByClassName("roof-images");
+  deckContent = document.getElementById("deck-gallery"),
+  interiorContent = document.getElementById("interior-gallery"),
+  modalContainer = document.getElementById("modal-container"),
+  innerCarousel = document.getElementById("inner-carousel"),
+  leftArrow = document.getElementById("left"),
+  rightArrow = document.getElementById("right"),
+  slideImages = document.getElementsByClassName("mySlides"),
+  modalContainerD = document.getElementById("modal-container-deck"),
+  innerCarouselD = document.getElementById("inner-carousel-deck"),
+  leftArrowD = document.getElementById("left-deck"),
+  rightArrowD = document.getElementById("right-deck"),
+  slideImagesD = document.getElementsByClassName("mySlidesD"),
+  modalContainerI = document.getElementById("modal-container-interior"),
+  innerCarouselI = document.getElementById("inner-carousel-interior"),
+  leftArrowI = document.getElementById("left-interior"),
+  rightArrowI = document.getElementById("right-interior"),
+  slideImagesI = document.getElementsByClassName("mySlidesI"),
+  roofImageList = document.getElementsByClassName("roof-images");
 
 
 /* Display all images for roofing */
 for (let i = 0; i < roofImages.length; i++) {
-    roofingContent.innerHTML +=
-        `
+  roofingContent.innerHTML +=
+    `
     <article class="col-6 col-md-4 col-lg-3">
-    <img class="roof-images" onclick="modalShow(); currentSlide(${i+1})" src="${roofImages[i].source}" alt="${roofImages[i].alt}">
+    <img class="roof-images" onclick="modalShow(); currentSlide(${i + 1})" src="${roofImages[i].source}" alt="${roofImages[i].alt}">
     </article>
     `;
 }
 
 /* Display all images for deck and fences */
 for (let i = 0; i < deckImages.length; i++) {
-    deckContent.innerHTML +=
-        `
+  deckContent.innerHTML +=
+    `
     <article class="col-6 col-md-4 col-lg-3">
-    <img class="deck-images" onclick="modalShowD(); currentSlideD(${i+1})" src="${deckImages[i].source}" alt="${deckImages[i].alt}">
+    <img class="deck-images" onclick="modalShowD(); currentSlideD(${i + 1})" src="${deckImages[i].source}" alt="${deckImages[i].alt}">
     </article>
     `;
 }
 
 /* Display all images for interior renovations*/
 for (let i = 0; i < interiorImages.length; i++) {
-    interiorContent.innerHTML +=
-        `
+  interiorContent.innerHTML +=
+    `
     <article class="col-6 col-md-4 col-lg-3">
-    <img class="interior-images" onclick="modalShowI(); currentSlideI(${i+1})" src="${interiorImages[i].source}" alt="${interiorImages[i].alt}">
+    <img class="interior-images" onclick="modalShowI(); currentSlideI(${i + 1})" src="${interiorImages[i].source}" alt="${interiorImages[i].alt}">
     </article>
     `;
 }
@@ -52,43 +52,88 @@ for (let i = 0; i < interiorImages.length; i++) {
 /* MODAL */
 
 function modalShow() {
-    modalContainer.style.display = "block";
+  modalContainer.style.display = "block";
 }
 
 function modalShowD() {
-    modalContainerD.style.display = "block";
+  modalContainerD.style.display = "block";
 }
 
 function modalShowI() {
-    modalContainerI.style.display = "block";
+  modalContainerI.style.display = "block";
 }
 
 $(document).on('click', '#exit', function () {
-    //do something
-    modalContainer.style.display = "none";
+  //do something
+  modalContainer.style.display = "none";
 })
 
 $(document).on('click', '#exit-deck', function () {
-    //do something
-    modalContainerD.style.display = "none";
+  //do something
+  modalContainerD.style.display = "none";
 })
 
 $(document).on('click', '#exit-interior', function () {
-    //do something
-    modalContainerI.style.display = "none";
+  //do something
+  modalContainerI.style.display = "none";
 })
 
 
 for (let i = 0; i < roofImages.length; i++) {
-    innerCarousel.innerHTML += `<img class="mySlides" src="${roofImages[i].source}" alt="${roofImages[i].alt}"></img>`;
+  let caption;
+
+  if(roofImages[i].caption == undefined){
+    caption = " ";
+  } else {
+    caption = roofImages[i].caption;
+  }
+
+  innerCarousel.innerHTML +=
+    `
+    <div class="mySlides text-center">
+    <img src="${roofImages[i].source}" alt="${roofImages[i].alt}">
+    <span">${i + 1}/${roofImages.length}</span>
+    <h6>${caption}</h6>
+    </div>
+    `;
+
 }
 
 for (let i = 0; i < deckImages.length; i++) {
-    innerCarouselD.innerHTML += `<img class="mySlidesD" src="${deckImages[i].source}" alt="${deckImages[i].alt}"></img>`;
+
+  let caption;
+
+  if(deckImages[i].caption == undefined){
+    caption = " ";
+  } else {
+    caption = deckImages[i].caption;
+  }
+
+  innerCarouselD.innerHTML += `
+    <div class="mySlidesD text-center">
+    <img src="${deckImages[i].source}" alt="${deckImages[i].alt}">
+    <span">${i + 1}/${deckImages.length}</span>
+    <h6>${caption}</h6>
+    `;
 }
 
 for (let i = 0; i < interiorImages.length; i++) {
-    innerCarouselI.innerHTML += `<img class="mySlidesI" src="${interiorImages[i].source}" alt="${interiorImages[i].alt}"></img>`;
+
+  let caption;
+
+  if(interiorImages[i].caption == undefined){
+    caption = " ";
+  } else {
+    caption = interiorImages[i].caption;
+  }
+
+  innerCarouselI.innerHTML += `
+    <div class="mySlidesI text-center">
+    <img src="${interiorImages[i].source}" alt="${interiorImages[i].alt}">
+    <span">${i + 1}/${interiorImages.length}</span>
+    <h6>${caption}</h6>
+    </div>
+    `;
 }
 
 /* Roof */
@@ -109,12 +154,12 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
 }
 
 /* Deck */
@@ -135,12 +180,12 @@ function currentSlideD(d) {
 function showSlidesD(d) {
   var i;
   var slides = document.getElementsByClassName("mySlidesD");
-  if (d > slides.length) {slideIndexD = 1}
-  if (d < 1) {slideIndexD = slides.length}
+  if (d > slides.length) { slideIndexD = 1 }
+  if (d < 1) { slideIndexD = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndexD-1].style.display = "block";
+  slides[slideIndexD - 1].style.display = "block";
 }
 
 /* Interior */
@@ -161,25 +206,25 @@ function currentSlideI(i) {
 function showSlidesI(i) {
   var x;
   var slides = document.getElementsByClassName("mySlidesI");
-  if (i > slides.length) {slideIndexI = 1}
-  if (i < 1) {slideIndexI = slides.length}
+  if (i > slides.length) { slideIndexI = 1 }
+  if (i < 1) { slideIndexI = slides.length }
   for (x = 0; x < slides.length; x++) {
     slides[x].style.display = "none";
   }
-  slides[slideIndexI-1].style.display = "block";
+  slides[slideIndexI - 1].style.display = "block";
 }
 
 $(window).keyup(function (e) {
   var key = e.which;
-  if(key == 37 ) {
+  if (key == 37) {
     plusSlides(-1);
     plusSlidesD(-1);
     plusSlidesI(-1);
-  } else if(key == 39) {
+  } else if (key == 39) {
     plusSlides(1);
     plusSlidesD(1);
-    plusSlidesI(1); 
-  } else if(key == 27) {
+    plusSlidesI(1);
+  } else if (key == 27) {
     modalContainer.style.display = "none";
     modalContainerD.style.display = "none";
     modalContainerI.style.display = "none";
@@ -187,9 +232,9 @@ $(window).keyup(function (e) {
 });
 
 window.onclick = function (event) {
-    if (event.target == modalContainer || event.target == modalContainerD || event.target == modalContainerI ) {
-        modalContainer.style.display = "none";
-        modalContainerD.style.display = "none";
-        modalContainerI.style.display = "none";
-    }
+  if (event.target == modalContainer || event.target == modalContainerD || event.target == modalContainerI) {
+    modalContainer.style.display = "none";
+    modalContainerD.style.display = "none";
+    modalContainerI.style.display = "none";
+  }
 }
