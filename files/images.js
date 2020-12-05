a242c283e094437e3ed50c1cc045b7b60d02acd4
Kim Zuniga <kim.eli.zuniga@gmail.com>
2020-12-04 20:22:23 -0500

    Change photos

--- files/images.js
+++ files/images.js
@@ -1,24 +1,22 @@
 /* Array of images' sources + alt */
 
 let roofImages = [
-    {"source": "images/roof_1.jpg", "alt": "roof_1", "caption": "Metal roof"},
-    {"source": "images/roof_2.jpg", "alt": "roof_2"},
-    {"source": "images/roof_3.jpg", "alt": "roof_3"}
+    {"source": "images/roof_4.jpg", "alt": "roof_4"},
+    {"source": "images/roof_5.jpg", "alt": "roof_5"}
 ]
 
 let deckImages = [
-    {"source": "images/deck_1.jpg", "alt": "deck_1"},
-    {"source": "images/deck_2.jpg", "alt": "deck_2"},
-    {"source": "images/deck_4.jpg", "alt": "deck_4", "caption": "Deck built with blah blah blah"},
+    {"source": "images/deck_4.jpg", "alt": "deck_4"},
     {"source": "images/deck_5.jpg", "alt": "deck_5"},
     {"source": "images/deck_6.jpg", "alt": "deck_6"},
-    {"source": "images/fence_1.jpg", "alt": "fence_1"},
-    {"source": "images/fence_2.jpg", "alt": "fence_2"}
+    {"source": "images/deck_7.jpg", "alt": "deck_7"},
+    {"source": "images/deck_8.jpg", "alt": "deck_8"},
+    {"source": "images/deck_9.jpg", "alt": "deck_9"},
+    {"source": "images/front_steps2.jpg", "alt": "front_steps2"},
 ]
 
 let interiorImages = [
-    {"source": "images/kitchen_1.jpg", "alt": "kitchen_1"},
-    {"source": "images/bath_1.jpg", "alt": "bath_1"},
-    {"source": "images/basement_1.jpg", "alt": "basement_1"},
-    {"source": "images/floor_1.jpg", "alt": "floor_1"},
+    {"source": "images/living_room_1.jpg", "alt": "living_room_1"},
+    {"source": "images/stairs_1.jpg", "alt": "stairs_1"},
+    {"source": "images/stairs_2.jpg", "alt": "stairs_2"},
 ]
