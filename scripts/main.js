4bc757747c08762d885b7ef8b5661b8e41633971
Kim Zuniga <kim.eli.zuniga@gmail.com>
2021-04-06 23:03:19 -0400

    Remove roofing

--- scripts/main.js
+++ scripts/main.js
@@ -20,15 +20,7 @@ let roofingContent = document.getElementById("roofing-gallery"),
   roofImageList = document.getElementsByClassName("roof-images");
 
 
-/* Display all images for roofing */
-for (let i = 0; i < roofImages.length; i++) {
-  roofingContent.innerHTML +=
-    `
-    <article class="col-6 col-md-4 col-lg-3">
-    <img class="roof-images" onclick="modalShow(); currentSlide(${i + 1})" src="${roofImages[i].source}" alt="${roofImages[i].alt}">
-    </article>
-    `;
-}
+
 
 /* Display all images for deck and fences */
 for (let i = 0; i < deckImages.length; i++) {
@@ -54,11 +46,6 @@ for (let i = 0; i < interiorImages.length; i++) {
 
 let modalBackgroundSource;
 
-function modalShow() {
-  modalContainer.style.display = "block";
-  document.body.style.overflow = "hidden";
-}
-
 function modalShowD() {
   modalContainerD.style.display = "block";
   document.body.style.overflow = "hidden";
@@ -68,12 +55,6 @@ function modalShowI() {
   modalContainerI.style.display = "block";
   document.body.style.overflow = "hidden";
 }
-
-$(document).on('click', '#exit', function () {
-  //do something
-  modalContainer.style.display = "none";
-  document.body.style.overflow = "auto";
-})
 
 $(document).on('click', '#exit-deck', function () {
   //do something
@@ -87,25 +68,6 @@ $(document).on('click', '#exit-interior', function () {
   document.body.style.overflow = "auto";
 })
 
-
-for (let i = 0; i < roofImages.length; i++) {
-  let caption;
-
-  if(roofImages[i].caption == undefined){
-    caption = " ";
-  } else {
-    caption = roofImages[i].caption;
-  }
-
-  innerCarousel.innerHTML +=
-    `
-    <div class="mySlides text-center">
-    <img src="${roofImages[i].source}" alt="${roofImages[i].alt}">
-    <span">${i + 1}/${roofImages.length}</span>
-    <h6>${caption}</h6>
-    </div>
-    `;
-}
 
 
 
@@ -147,20 +109,6 @@ for (let i = 0; i < interiorImages.length; i++) {
     `;
 }
 
-/* Roof */
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
 function showSlides(n) {
   var i;
@@ -247,23 +195,19 @@ function showSlidesI(i) {
 $(window).keyup(function (e) {
   var key = e.which;
   if (key == 37) {
-    plusSlides(-1);
     plusSlidesD(-1);
     plusSlidesI(-1);
   } else if (key == 39) {
-    plusSlides(1);
     plusSlidesD(1);
     plusSlidesI(1);
   } else if (key == 27) {
-    modalContainer.style.display = "none";
     modalContainerD.style.display = "none";
     modalContainerI.style.display = "none";
   }
 });
 
 window.onclick = function (event) {
-  if (event.target == modalContainer || event.target == modalContainerD || event.target == modalContainerI) {
-    modalContainer.style.display = "none";
+  if ( event.target == modalContainerD || event.target == modalContainerI) {
     modalContainerD.style.display = "none";
     modalContainerI.style.display = "none";
     document.body.style.overflow = "auto";
