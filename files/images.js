23a10111ebe0b41a7698eebd72d60e778f4f0b19
Kim Zuniga <kim.eli.zuniga@gmail.com>
2021-09-18 16:56:21 -0400

    Delete 2 more photos

--- files/images.js
+++ files/images.js
@@ -2,7 +2,6 @@
 
 let deckImages = [
     {"source": "images/deck_001.jpg", "alt": "deck"},
-    {"source": "images/deck_003.jpg", "alt": "deck"},
     {"source": "images/deck_004.jpg", "alt": "deck"},
     {"source": "images/deck_006.jpg", "alt": "deck"},
     {"source": "images/deck_007.jpg", "alt": "deck"},
@@ -11,7 +10,6 @@ let deckImages = [
 
 let interiorImages = [
     {"source": "images/interior_001.jpg", "alt": "interior"},
-    {"source": "images/interior_002.jpg", "alt": "interior"},
     {"source": "images/interior_003.jpg", "alt": "interior"},
     {"source": "images/interior_004.jpg", "alt": "interior"},
     {"source": "images/interior_005.jpg", "alt": "interior"}
